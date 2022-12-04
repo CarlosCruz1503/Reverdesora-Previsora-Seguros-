c5535a3ecb3ff18250339f23f846049ee2f5c5f4
CarlosCruz1503 <carlosjose445566@gmail.com>
2022-12-03 23:59:12 -0500

    commit final ?

--- src/components/Claves.vue
+++ src/components/Claves.vue
@@ -5,10 +5,10 @@
             <h1 class="c-h1">Lo que te ofrecemos</h1>
         </div>
         <div class="row">
-            <div class="col-12 col-md-6 c-img">
+            <div class="col-12 col-lg-6 c-img">
                 <img src="../../public/img/5250872-1080x675.jpg" alt="">
             </div>
-            <div class="col-12 col-md-6 c-claves">
+            <div class="col-12 col-lg-6 c-claves">
                 <CardClave title="1. Bonos" text="El valor de las primas serán invertidas en bonos verdes que serán destinados a sus propios proyectos que generen impacto positivo en el medio ambiente
                 " icon="bi bi-tree"/>
                 <CardClave title="2. Ajustable" text="Tanto las coberturas como las asistencias se acomodan a las iniciativas propuestas, el valor de la prima considera el valor del portafolio completo para aplicar tasas preferenciales.
--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -5,34 +5,39 @@
             <h1 class="f-h1">Preguntas frecuentes</h1>
         </div>
         <div class="faq">
-            <div class="accordion" id="accordionExample">
-                <FaqQ name="pregunta" nameNumber="pregunta1" ANameNumber="#pregunta1" FaqT="¿Qué es reverdesora?"
-                    Answer="Reverdesora es un paquete de seguros ajustado a las necesidades de los clientes y en línea con las iniciativas que conforman cada portafolio de iniciativas ASG.
-                " />
-                <FaqQ name="preguntaa" nameNumber="pregunta2" ANameNumber="#pregunta2"
-                    FaqT="¿Qué pasá si invierto y de repente suben los precios de generación de energía renovable?"
-                    Answer="La previsora te asegura tus fondos y que con la inversión inicial tu proyecto se finalice, Previsora responde.. Tú tranquilo!!!  " />
-                <FaqQ name="preguntaaa" nameNumber="pregunta3" ANameNumber="#pregunta3"
-                    FaqT="¿Hacia quiénes va dirigido este seguro?"
-                    Answer="Empresas que cumplan con los criterios de elegibilidad de la taxonomía verde" />
-                <FaqQ name="preguntaaaa" nameNumber="pregunta4" ANameNumber="#pregunta4"
-                    FaqT="¿Por qué existe este seguro?" Answer="“Reverdesora”, es el primer seguro verde en Colombia y en el mundo creado para proteger a las organizaciones que cumplen con los criterios de elegibilidad de la taxonomía verde, durante la implementación de sus portafolios de iniciativas ASG orientadas a reducir el riesgo de sostenibilidad es decir, aquellos riesgos que por circunstancias ambientales, sociales o de gobernanza, produzcan un impacto adverso al desarrollo económico ambientalmente sostenible.
-                    " />
-                <FaqQ name="preguntaaaaa" nameNumber="pregunta5" ANameNumber="#pregunta5" FaqT="¿Qué cubre este seguro?"
-                    Answer="Ejemplo de coberturas:
-                    Responsabilidad Civil por contaminación de predios, 
-                    Seriedad de oferta, 
-                    Buen manejo y correcta inversión del anticipo, 
-                    Cumplimiento, 
-                    Pago de salarios, 
-                    Estabilidad de obra, entre muchos otros 
-                    
-                    " />
-                <FaqQ name="preguntaaaaaa" nameNumber="pregunta6" ANameNumber="#pregunta6"
-                    FaqT="¿Comó se calcula la prima?" Answer="El portafolio ASG es valorizado y partir del alcance definido para cada una de las iniciativas y horizontes, se estimará el paquete de coberturas adecuado y así mismo el valor de la prima a pagar.
+            <div class="row">
+                <div class="col-12 col-md-2"></div>
+                <div class="col-12 col-md-8">
+                    <div class="accordion" id="accordionExample">
+                        <FaqQ name="pregunta" nameNumber="pregunta1" ANameNumber="#pregunta1"
+                            FaqT="¿Qué es reverdesora?" Answer="Reverdesora es un paquete de seguros ajustado a las necesidades de los clientes y en línea con las iniciativas que conforman cada portafolio de iniciativas ASG.
+                        " />
+                        <FaqQ name="preguntaa" nameNumber="pregunta2" ANameNumber="#pregunta2"
+                            FaqT="¿Qué pasá si invierto y de repente suben los precios de generación de energía renovable?"
+                            Answer="La previsora te asegura tus fondos y que con la inversión inicial tu proyecto se finalice, Previsora responde.. Tú tranquilo!!!  " />
+                        <FaqQ name="preguntaaa" nameNumber="pregunta3" ANameNumber="#pregunta3"
+                            FaqT="¿Hacia quiénes va dirigido este seguro?"
+                            Answer="Empresas que cumplan con los criterios de elegibilidad de la taxonomía verde" />
+                        <FaqQ name="preguntaaaa" nameNumber="pregunta4" ANameNumber="#pregunta4"
+                            FaqT="¿Por qué existe este seguro?" Answer="“Reverdesora”, es el primer seguro verde en Colombia y en el mundo creado para proteger a las organizaciones que cumplen con los criterios de elegibilidad de la taxonomía verde, durante la implementación de sus portafolios de iniciativas ASG orientadas a reducir el riesgo de sostenibilidad es decir, aquellos riesgos que por circunstancias ambientales, sociales o de gobernanza, produzcan un impacto adverso al desarrollo económico ambientalmente sostenible.
+                            " />
+                        <FaqQ name="preguntaaaaa" nameNumber="pregunta5" ANameNumber="#pregunta5"
+                            FaqT="¿Qué cubre este seguro?" Answer="Ejemplo de coberturas:
+                            Responsabilidad Civil por contaminación de predios, 
+                            Seriedad de oferta, 
+                            Buen manejo y correcta inversión del anticipo, 
+                            Cumplimiento, 
+                            Pago de salarios, 
+                            Estabilidad de obra, entre muchos otros 
+                            
+                            " />
+                        <FaqQ name="preguntaaaaaa" nameNumber="pregunta6" ANameNumber="#pregunta6"
+                            FaqT="¿Comó se calcula la prima?"
+                            Answer="El portafolio ASG es valorizado y partir del alcance definido para cada una de las iniciativas y horizontes, se estimará el paquete de coberturas adecuado y así mismo el valor de la prima a pagar." />
+                    </div>
+                    <div class="col-12 col-md-2"></div>
+                </div>
 
-                    
-                    " />
             </div>
         </div>
     </div>
